bcce9489804501bde82f5f295fc0422923c2825f
tastefulKeypad <lpok@mail.ru>
2026-01-11 20:55:09 +0300

    Added scheduled tasks for automatic payment

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from datetime import datetime, timedelta, timezone
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy.orm import Session
-import os, sys
 from pwdlib import PasswordHash
+from contextlib import asynccontextmanager
+from typing import Annotated, List
+import asyncio
+import schedule 
+import time
+import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
+# Imports from local files
 import db
 import schemas
 import endpoints
-from db.database import Base, engine
+from db.database import Base, engine, SessionLocal
 import db.database as appdb
 import db.models as models
+import schemas.user, schemas.transaction, schemas.sub
+from endpoints import fakePayment
 from endpoints.user import router as user_router
 from endpoints.auth import router as token_router
 from endpoints.product import router as product_router
@@ -19,15 +28,13 @@ from endpoints.sub import router as sub_router
 from endpoints.transaction import router as transaction_router
 from endpoints.refund import router as refund_router
 
-# Init JWT token data 
-SECRET_KEY = "CHANGE_ME"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+# How often scheduled task runs in seconds
+SCHEDULED_TASK_TIME = 10
 
-# Create database tables
+################# Init FastAPI app #################
+# Create database tables if they dont exist yet
 Base.metadata.create_all(bind=engine)
 
-# Init FastAPI app
 security = HTTPBasic()
 app = FastAPI()
 app.include_router(user_router)
@@ -38,7 +45,94 @@ app.include_router(sub_router)
 app.include_router(transaction_router)
 app.include_router(refund_router)
 
-# Endpoints
+################# Scheduled tasks #################
+async def process_pending_transactions():
+    db = appdb.SessionLocal()
+    db_transactions = db.query(models.Transaction).filter(
+        models.Transaction.userId == 0
+    ).all()
+
+    # Try to process every pending transaction
+    for transaction in db_transactions:
+        price_final = transaction.bankChange
+        if transaction.action == "Refund request":
+            price_final = transaction.toRefund
+        payment_service_response = fakePayment.fake_payment_method(price_final)
+
+        # Update db only on successfull payment
+        if payment_service_response.status == "Success":
+            new_transaction = models.Transaction(
+                userId = transaction.hiddenUserId,
+                productId = transaction.productId,
+                action = transaction.action,
+                dateTime = datetime.now(timezone.utc),
+                status = "Success",
+            )
+            
+            if transaction.action == "Refund request":
+                transaction.toRefund = payment_service_response.bankChange
+            else:
+                transaction.bankChange = payment_service_response.bankChange
+
+            db.delete(transaction)
+            db.add(new_transaction)
+    db.commit()
+    db.close()
+
+# Automatically cancel all subscriptions that expired for longer than 3 days
+# async def cancel_expired_subscriptions():
+
+# Check expired subscriptions and try to automatically refresh them
+'''
+async def add_new_pending_transactions():
+    db = appdb.SessionLocal()
+    db_transactions = db.query(models.Sub).filter(
+        models.Transaction.userId == 0
+    ).all()
+
+    # Try to process every pending transaction
+    for transaction in db_transactions:
+        price_final = transaction.bankChange
+        if transaction.action == "Refund request":
+            price_final = transaction.toRefund
+        payment_service_response = fakePayment.fake_payment_method(price_final)
+
+        # Update db only on successfull payment
+        if payment_service_response.status == "Success":
+            new_transaction = models.Transaction(
+                userId = transaction.hiddenUserId,
+                productId = transaction.productId,
+                action = transaction.action,
+                dateTime = datetime.now(timezone.utc),
+                status = "Success",
+            )
+            
+            if transaction.action == "Refund request":
+                transaction.toRefund = payment_service_response.bankChange
+            else:
+                transaction.bankChange = payment_service_response.bankChange
+
+            db.delete(transaction)
+            db.add(new_transaction)
+    db.commit()
+    db.close()
+'''
+
+async def periodic_task():
+    while True:
+        print("Processing pending payments")
+        #await add_new_pending_transactions()
+        await process_pending_transactions()
+        #await cancel_expired_subscriptions()
+        await asyncio.sleep(SCHEDULED_TASK_TIME)
+
+@app.on_event("startup")
+async def app_startup():
+    # Start the periodic task
+    asyncio.create_task(periodic_task())
+
+
+################# Endpoints #################
 @app.get("/")
 def read_root():
     return {"message": "Public endpoint"}
