d0e9dc61158677271938ee37f2fffd632c2ab1c5
Jesspern <andrey.bobovich2003@gmail.com>
2026-01-11 19:01:43 +0300

    minor edit

--- securityConfig.py
+++ securityConfig.py
@@ -1,7 +1,7 @@
 # JWT token generation data
 SECRET_KEY = "a0c684ef7e4ba7ab2dfbf92591dc8bf8800891d9e04caeccc975b26c84fa587d"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30\
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # Data for sendenig messages
 SMTP_SERVER = "smtp.gmail.com"
